d60502f5449bccfb549546487cd236d0146dafb5
pur3um <99007025+pur3um@users.noreply.github.com>
2022-03-19 10:38:37 +0900

    [Update] ✍ programmers_level2.튜플

--- Week06/튜플/튜플_pr.py
+++ Week06/튜플/튜플_pr.py
@@ -1,16 +1,19 @@
-# 순서가 있음
+# replace 여러개 : maketrans
 def solution(s):
-    s = s.replace('{','[',s.count('{'))
-    s = s.replace('}',']',s.count('}'))
-    s = sorted(eval(s), key=lambda x: len(x))#[[2], [2, 1], [2, 1, 3], [2, 1, 3, 4]]
+    s = s.replace('{', '[') #,s.count('{') : 바꿀 횟수 but 소용이 없었다..
+    s = s.replace('}', ']', s.count('}'))
+    s = sorted(eval(s), key=lambda x: len(x)) # [[2], [2, 1], [2, 1, 3], [2, 1, 3, 4]]
     result = []
     for i in s:
         set_i = set(i)
-        if len(result) == 0:
-            result += i
+        if len(result) == 0: # set으로 교집합을 진행, 아무 것도 없을 때 추가
+            # result += i
+            result.extend(i)
         else:
             set_result = set(result)
-            add = list(set_i - set_result)
+            add = list(set_i - set_result) # 이미 들어가 있는 것을 제외하고 추가
             result += add
 
     return result
+
+solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")
