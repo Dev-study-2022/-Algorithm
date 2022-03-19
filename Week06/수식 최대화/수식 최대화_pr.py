160d27ee3d0e2ec3144e69642eeb1303bc77b291
pur3um <99007025+pur3um@users.noreply.github.com>
2022-03-19 10:43:34 +0900

    [Update] ✍ programmers_level2.수식 최대화

--- Week06/수식 최대화/수식 최대화_pr.py
+++ Week06/수식 최대화/수식 최대화_pr.py
@@ -1,45 +1,15 @@
+# 다른 분꺼 참조했던 기억이 있음
 def solution(expression):
-    operator = ['+', '-', '*']
-    combination = [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]]
-    nums, operation, result = [], [], []
-    pointer = 0
-
-    for i, e in enumerate(expression) :
-        if e in operator :
-            nums.append(int(expression[pointer:i]))
-            operation.append(e)
-            pointer = i + 1
-    nums.append(int(expression[pointer:]))
-
-    for combi in combination:
-        numsArr = nums[:]
-        opArr = operation[:]
-        for c in combi :
-            i = 0
-            while(len(numsArr)>1 and operator[c] in opArr) :
-                op = opArr[i]
-                if op == operator[c] :
-                    del opArr[i]
-                    num1, num2 = numsArr[i:i+2]
-                    del numsArr[i:i+2]
-                    if c == 0 : numsArr.insert(i,num1+num2)
-                    elif c == 1 : numsArr.insert(i,num1-num2)
-                    else : numsArr.insert(i,num1*num2)
-                else : i += 1
-        result.append(abs(numsArr[0]))
-    return sorted(result)[-1]
-
-# 다른 사람 풀이
-def solution(expression):
+    # 모든 경우의 수를 그냥 지정
     operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
     answer = []
     for op in operations:
-        a = op[0]
-        b = op[1]
+        a = op[0] # 첫번째꺼로 분리
+        b = op[1] # 두번째꺼로 분리
         temp_list = []
         for e in expression.split(a):
             temp = [f"({i})" for i in e.split(b)]
             temp_list.append(f'({b.join(temp)})')
-        answer.append(abs(eval(a.join(temp_list))))
+        answer.append(abs(eval(a.join(temp_list)))) # 나머지는 분리할 필요 없이 eval로 계산하여 append
     return max(answer)
 
